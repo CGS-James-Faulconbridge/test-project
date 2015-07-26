7d404d2e975033aaf0159918660b98c9b29b7a23
Steele Butler <1641@cgs.act.edu.au>
2015-07-27 09:28:12 +1000

    branch check

--- testXcode/ViewController.swift
+++ testXcode/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //branch chceck
+        
         //Debug Reasons
         println("hello World")
     }
